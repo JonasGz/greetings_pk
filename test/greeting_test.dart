399c2a7d5d0c7b00737585c6b785df0d2009dcf5
JonasGz <you@example.com>
2024-06-22 16:18:00 -0300

    change name package

--- test/greeting_test.dart
+++ test/greeting_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:greeting/greetingpk.dart';
+import 'package:greetingpk/greetingpk.dart';
 
 void main() {
   group('Teste de saudação', () {
